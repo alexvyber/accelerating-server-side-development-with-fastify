aecf33d759243d378d57b17e0a01d3366cd96742
Alexey Sokolov <alexvyber@gmail.com>
2023-07-28 09:58:25 +0300

    page 100 - The onReady hook

--- working/4-chapter/on-route.cjs
+++ working/4-chapter/on-route.cjs
@@ -1,38 +1,44 @@
-const Fastify = require('fastify')
-const app = Fastify({ logger: true })
+const Fastify = require("fastify");
+const app = Fastify({ logger: true });
 
-app.addHook('onRoute', routeOptions => { // [1]
-  async function customPreHandler (request, reply) {
-    request.log.info('Hi from customPreHandler!')
+app.addHook("onRoute", (routeOptions) => {
+  // [1]
+  async function customPreHandler(request, reply) {
+    request.log.info("Hi from customPreHandler!");
   }
-  app.log.info('Adding custom preHandler to the route.')
-  routeOptions.preHandler = [...(routeOptions.preHandler ?? []), customPreHandler] // [2]
-})
+
+  app.log.info("Adding custom preHandler to the route.");
+  routeOptions.preHandler = [
+    ...(routeOptions.preHandler ?? []),
+    customPreHandler,
+  ]; // [2]
+});
+
 app.route({
   // [3]
-  url: '/foo',
-  method: 'GET',
+  url: "/foo",
+  method: "GET",
   schema: {
     200: {
-      type: 'object',
+      type: "object",
       properties: {
         foo: {
-          type: 'string'
-        }
-      }
-    }
+          type: "string",
+        },
+      },
+    },
   },
   handler: (req, reply) => {
-    reply.send({ foo: 'bar' })
-  }
-})
+    reply.send({ foo: "bar" });
+  },
+});
 
 app
   .listen({ port: 3000 })
   .then(() => {
-    app.log.info('Application is listening.')
+    app.log.info("Application is listening.");
   })
-  .catch(err => {
-    app.log.error(err)
-    process.exit()
-  })
+  .catch((err) => {
+    app.log.error(err);
+    process.exit();
+  });
