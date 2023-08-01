073bf91e5a89c4e3f40dcd53a4baf0f217f700c8
Alexey Sokolov <alexvyber@gmail.com>
2023-08-01 09:59:46 +0300

    page 161 - Loading the routes

---
+++ working/fastify-app/app.js
@@ -0,0 +1,36 @@
+"use strict"
+
+const path = require("path")
+const AutoLoad = require("@fastify/autoload")
+
+// Pass --options via CLI arguments in command to enable these options.
+module.exports.options = {}
+
+module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "plugins"),
+    ignorePattern: /.*.no-load\.js/,
+    indexPattern: /^no$/i,
+    options: Object.assign({}, opts),
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  })
+
+  // Schema loading
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "schemas"),
+    indexPattern: /^loader.js$/i,
+  })
+}
